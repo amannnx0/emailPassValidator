c51c15bbb527ca68dd43f65caf7f1eba1340b0cc
Aman <amanraj06a@gmail.com>
2024-07-10 22:59:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function validateInput(obj){
 
   const schmea = zod.object({
     email : zod.string().email(),
-    password : zod.string().min(5)
+    password : zod.string().min(5),
+    name : zod.string().name()
   })
 
 const response = schmea.safeParse(obj);
@@ -15,4 +16,5 @@ console.log(response);
 validateInput({
   email : "amanraj06@agmail.com",
   password : "fsssds"
+  name : "LTIMINDTREE"
 })
